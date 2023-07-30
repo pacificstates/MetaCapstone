2eb0420e8c069afb9697d311ccfe76193d6a1e4a
Jacob Hilt <jacob@Jacobs-Mac-mini.local>
2023-07-30 15:56:39 +0700

    changed from pipenv to venv

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reservation',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
